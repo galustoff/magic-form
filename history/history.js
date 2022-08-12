a58f2828a2181c2dc0dff9b72c7dd3972af32551
galustoff <galustoff.dan@yandex.ru>
2022-08-12 22:11:58 +0300

    realize removing cards from storage

--- history/history.js
+++ history/history.js
@@ -2,17 +2,40 @@ const ls = localStorage
 const container = document.querySelector('.main__container')
 const template = document.getElementById('card-template')
 
-if (ls.history) {
+renderCards()
+
+function renderCards() {
+  if (ls.history) {
+    const history = JSON.parse(ls.history)
+
+    for (let key in history) {
+      const person = history[key]
+      const card = template.content.cloneNode(true)
+      const deleteBtn = card.querySelector('.delete-button')
+
+      card.querySelector('.card').setAttribute('id', key)
+
+      for (key in person) {
+        card.getElementById(key).textContent = person[key]
+      }
+
+      deleteBtn.addEventListener('click', deleteCard)
+      container.append(card)
+    }
+  }
+}
+
+function deleteCard(e) {
+  const item = e.target.closest('.card')
+
+  removeStorageItem(item.id)
+  container.removeChild(item)
+}
+
+function removeStorageItem(id) {
   const history = JSON.parse(ls.history)
 
-  for (let key in history) {
-    const person = history[key]
-    const card = template.content.cloneNode(true)
+  delete history[id]
 
-    for (key in person) {
-      card.getElementById(key).textContent = person[key]
-    }
-
-    container.append(card)
-  }
+  ls.history = JSON.stringify(history)
 }
